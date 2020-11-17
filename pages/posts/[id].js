7e876bb19d53681c278786765bee4b78038a316c
osakana0630 <osakana0630@gmail.com>
2020-11-18 06:27:16 +0900

    indexページに個別ページへのルーティング設定/表示スタイルの調整

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,25 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
-
 //パスを取得
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -24,7 +31,7 @@ export async function getStaticPaths() {
 
 //取得したパスごとのコンテンツを取得している
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
